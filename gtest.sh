be3a0245edef5f1ea9d1eae8a9cc4f7cb40dbe21
zhaofeng-shu33 <616545598@qq.com>
2019-11-16 11:59:13 +0800

    install gdb first

--- gtest.sh
+++ gtest.sh
@@ -4,7 +4,7 @@ set -e -x
 echo "deb http://ppa.launchpad.net/zhaofeng-shu33/gtest/ubuntu xenial main" > /etc/apt/sources.list.d/gtest.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B4552FED361B2FCCEAD7193C69FDF0C946D67581
 apt update
-apt install --yes g++ libgtest-dev cmake
+apt install --yes g++ libgtest-dev cmake gdb
 
 cat <<EOF >> gtest_example.cpp
 #include <gtest/gtest.h>
@@ -58,4 +58,4 @@ p a
 quit
 EOF
 
-gdb --batch ./fixtures
+gdb --batch --args ./fixtures --gtest_filter=A.power
